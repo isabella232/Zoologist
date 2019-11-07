04c0e5cc538270f2aff4998fbc16459d053a8fc5
AJGu <55819145+AJGu@users.noreply.github.com>
2019-11-06 16:38:19 -0800

    Add files via upload

--- zoologist.py
+++ zoologist.py
@@ -1,15 +1,16 @@
 from math import log
+from math import sqrt
 import csv
 
-def assignRankings(pop_growths):
+def assignRankings(equations):
     
     # rank values descending order
     rankings = []
-    while (not max(pop_growths) == -1):
-        nextValue = max(pop_growths)
-        index = pop_growths.index(nextValue) # find index
+    while (not max(equations) == -1):
+        nextValue = max(equations)
+        index = equations.index(nextValue) # find index
         rankings.append(index)
-        pop_growths[index] = -1
+        equations[index] = -1
 
     # get ascending order
     rankings.reverse()
@@ -69,7 +70,8 @@ def selectionSort(arr):
 
 
 
-def read(filename):
+def readFileintoDict():
+    filename= "Animals.txt"
     with open(filename, 'r') as file:
         result = {}
         index = 0
@@ -78,42 +80,53 @@ def read(filename):
             lastElement = noTabs[len(noTabs) - 1]
             result[index] = lastElement
             index += 1
-
-    print(result)
     return result
 
 def main():
     n = 1
-    pop_growths = [
-        n**2,       # 0
-        log(n),     # 1
-        n,          # 2
-        2**n,       # 3
-        n**3,       # 4
-        4*n,        # 5
-        1,          # 6
-        10,         # 7
-        4*n**3,     # 8
-        n*log(n),   # 9
-        n**n,       # 10
-        n**2+n**3,  # 11
-        n**4,       # 12
-        n**4+n**5   # 13
+    equations = [
+        n**2,                
+        20*n**2+5*n+7,           
+        n+log(n)+7,              
+        3**n,                   
+        log(n)/(n**2),       
+        (n+1)/(sqrt(1+n**2)),     
+        (n**n)/(sqrt(log(n**4))+10),      
+        n*log(n**2),       
+        6**n,     
+        sqrt(log(n)),   
+        n**n,      
+        n**3*log(n)+100, 
+        sqrt(n**3),     
+        n**3,
+        (n+1)/(sqrt(1+n**2))
     ]
 
-    animals = read("animalNames.txt") # animals = {index : animalName}
-    ranks = assignRankings(pop_growths.copy()) # ranks = {index : rank}
+    animals = readFileintoDict() # animals = {index : animalName}
+    ranks = assignRankings(equations.copy()) # ranks = {index : rank}
 
     for i in range(len(ranks)):
         print(animals[ranks[i]])
 
-    print("BUBBLE SORT")
-    bubbleSort(ranks
-               .copy())
-    print("INSERTION SORT")
-    insertionSort(ranks.copy())
-    print("SELECTION SORT")
-    selectionSort(ranks.copy())
+    invalidInput=True
+    while (invalidInput):
+        print("Please chose a sorting method:")
+        print("1.(Bubble) Sort")
+        print("2.(Insertion) Sort")
+        print("3.(Selection) Sort")
+        x=input("")
+        if(x=="1"or x=="bubble"):
+            bubbleSort(ranks.copy())
+            invalidInput=False
+        elif(x=="2"or x=="insertion"):
+            insertionSort(ranks.copy())
+            invalidInput=False
+        elif(x=="3"or x=="selection"):
+            selectionSort(ranks.copy())
+            invalidInput=False
+        else:
+            print("Not a valid input, try again.")
+        
 
 
 main()
